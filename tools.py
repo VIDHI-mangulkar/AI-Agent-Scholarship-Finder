3a65f2b0038fa714c7710495ea4847eb0eff75d5
VIDHI-mangulkar <vidhimangulkar@gmail.com>
2026-04-24 20:16:59 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,10 +1,10 @@
 import os
 from tavily import TavilyClient
-from dotenv import load_dotenv
 
-load_dotenv()
+# works in both local + streamlit cloud
+api_key = os.getenv("TAVILY_API_KEY")
 
-client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+client = TavilyClient(api_key=api_key)
 
 def search_scholarships(query):
 
